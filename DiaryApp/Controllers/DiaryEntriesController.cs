80134593d9a8f89460ed7de983a0cfba810386fa
Jonathan <jona.rierasanchez@gmail.com>
2026-01-02 00:54:30 +0100

    Done basic crud operations

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -12,11 +12,97 @@ namespace DiaryApp.Controllers
         {
             _db = db;
         }
+
         public IActionResult Index()
             //DiaryEntries = sql server table name
         {
             List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList();
             return View(objDiaryEntryList);
         }
-    }
-}
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(DiaryEntry obj)
+        {
+            //Server side validations
+            if (obj != null && obj.Title.Length < 3) {
+                ModelState.AddModelError("Title", "The title lengh is too short!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Add(obj); //Adding the new diary entry to the db
+                _db.SaveChanges(); //saving changes
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid? id) {
+
+            if(id == null || id == Guid.Empty)
+            {
+                return NotFound();
+            }
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+            //Server side validations
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "The title lengh is too short!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Update(obj); //Updating entry in db
+                _db.SaveChanges(); //saving changes
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(Guid? id)
+        {
+
+            if (id == null || id == Guid.Empty)
+            {
+                return NotFound();
+            }
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DiaryEntry obj)
+        {
+
+
+                _db.DiaryEntries.Remove(obj); //Remove entry from db
+                _db.SaveChanges(); //saving changes
+                return RedirectToAction("Index");
+        }
+     }
+ }
--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -10,12 +10,13 @@ namespace DiaryApp.Models
         //Anotations
         //[Key]
         public Guid Id { get; set; } = Guid.NewGuid();
-        [Required]
+        [Required(ErrorMessage = "Title can not be empty")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters!")]
         public string Title { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "Content can not be empty")]
         public string Content { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "The date can not be empty")]
         public DateTime Created { get; set; } = DateTime.MinValue;
 
     }
